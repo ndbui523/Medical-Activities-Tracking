2894925a754585ec1dda0315d126977441d2bf7b
Nick Bui <ndbui@live.unc.edu>
2016-11-03 17:43:56 -0400

    Added fields to the adviser view and linked the arrow in epa-details page to delta information from controllers.

--- epa-tracker/public/adviser/adviserCtrl.js
+++ epa-tracker/public/adviser/adviserCtrl.js
@@ -1,8 +1,92 @@
 angular.module('appControllers').controller('adviserCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
     $scope.adviser = $routeParams.adviser;
-    $scope.advisees = {
-      '720337498':'Lindsay Krinkle',
-      '720399847':'Jordan Clark',
-      '720337845':'Anna Brandenberg'
-    }
+    $scope.threshold = 3;
+    $scope.reverse = true;
+    $scope.property = 'regressed';
+
+    $scope.advisees = [
+        {
+          'firstName' : 'Lindsay',
+          'lastName' : 'Krinkle',
+          'email' : 'lkrinkle@fakeemail.com',
+          'grade' : 3,
+          'adviser' : 'Ana Felix',
+          'improved' : 2,
+          'regressed' : 1,
+        } ,
+        {
+          'firstName' : 'Gary',
+          'lastName' : 'Nicolson',
+          'email' : 'gnicolson@fakeemail.com',
+          'grade' : 1,
+          'adviser' : 'Ana Felix',
+          'improved' : 1,
+          'regressed' : 4,
+        } ,
+        {
+          'firstName' : 'Tod',
+          'lastName' : 'Radcliff',
+          'email' : 'tradcliff@fakeemail.com',
+          'grade' : 1,
+          'adviser' : 'Ana Felix',
+          'improved' : 3,
+          'regressed' : 2,
+        } ,
+        {
+          'firstName' : 'Jonnie',
+          'lastName' : 'Morris',
+          'email' : 'jmorris@fakeemail.com',
+          'grade' : 2,
+          'adviser' : 'Ana Felix',
+          'improved' : 3,
+          'regressed' : 0,
+        } ,
+        {
+          'firstName' : 'Austin',
+          'lastName' : 'Fern',
+          'email' : 'afern@fakeemail.com',
+          'grade' : 2,
+          'adviser' : 'Ana Felix',
+          'improved' : 1,
+          'regressed' : 1,
+        } ,
+        {
+          'firstName' : 'Paulene',
+          'lastName' : 'Myles',
+          'email' : 'pmyles@fakeemail.com',
+          'grade' : 2,
+          'adviser' : 'Ana Felix',
+          'improved' : 2,
+          'regressed' : 1,
+        } ,
+        {
+          'firstName' : 'Melissa',
+          'lastName' : 'Vernon',
+          'email' : 'mvernon@fakeemail.com',
+          'grade' : 3,
+          'adviser' : 'Ana Felix',
+          'improved' : 3,
+          'regressed' : 0,
+        } ,
+        {
+          'firstName' : 'Rita',
+          'lastName' : 'Jiggers',
+          'email' : 'rjiggers@fakeemail.com',
+          'grade' : 3,
+          'adviser' : 'Ana Felix',
+          'improved' : 0,
+          'regressed' : 3,
+        } ,
+    ];
+
+    $scope.sortBy = function(propertyName){
+      if ($scope.property == propertyName){
+        $scope.reverse = !$scope.reverse;
+      }
+      else{
+        $scope.reverse = true;
+      }
+      $scope.property = propertyName;
+
+    };
 }]);
--- epa-tracker/public/details/epa-details-controller.js
+++ epa-tracker/public/details/epa-details-controller.js
@@ -1,11 +1,56 @@
 angular.module('appControllers').controller('epa-details-controller', ['$scope','$routeParams','$http',function($scope,$routeParams,$http){
   $scope.epa = $routeParams.epa
+  $scope.deltaText = "";
+  $scope.deltaArrow = "even"
   $http({
   method: 'GET',
   url: '/details/' + $scope.epa
   }).then(function successCallback(response) {
     $scope.epaDetails=response.data
   }, function errorCallback(response) {
-    console.log("error")
+    console.log("Error reading in EPA details : /details/"+$scope.epa)
   });
+
+  $http({
+    method: 'GET',
+    url: '/users/1'
+    }).then(function successCallback(response) {
+      $scope.summaryDeltas=response.data.summaryDeltas;
+      if($.inArray(Number($scope.epa),$scope.summaryDeltas.Improved) != -1) {
+        $scope.deltaText = 'Your level in this EPA has risen since the last examination.'
+        $scope.deltaArrow = 'up'
+      }
+      else if($.inArray(Number($scope.epa),$scope.summaryDeltas.Even) != -1) {
+        $scope.deltaText = 'Your level in this EPA has stayed the same since the last examination.';
+        $scope.deltaArrow = 'even'
+      }
+      else {
+        $scope.deltaText = 'Your level in this EPA has fallen since the last examination.';
+        $scope.deltaArrow = 'down'
+      }
+    }, function errorCallback(response) {
+      console.log("Error in /users/1")
+    });
+
+    $scope.testInfo = [
+      {
+        'name' : 'May 05 2016 - Test DEF: 95%',
+        'delta' : 'up'
+      },
+
+      {
+        'name' : 'April 25 2016 - Test GHI: 79%',
+        'delta' : 'even'
+      },
+
+      {
+        'name' : 'Jan 09 2016 - Test XYZ: 65%',
+        'delta' : 'up'
+      },
+
+      {
+        'name' : 'Sept 14 2015 - Test ABC: 90%',
+        'delta' : 'down'
+      }
+    ];
 }]);
